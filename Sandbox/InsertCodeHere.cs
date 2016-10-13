220e433548c36d072401a897fc9b97d725161b3f
Søren Roed Schack <soer622t@edu.easj.dk>
2016-10-13 13:08:43 +0200

    case 4 omskrevet fra while- til for-loop

--- Sandbox/InsertCodeHere.cs
+++ Sandbox/InsertCodeHere.cs
@@ -31,23 +31,27 @@ namespace Sandbox
             Console.WriteLine("Start of case 3");
             
             for (int c3 = 0; c3 < 12; c3 += 2)
-
+                Console.WriteLine(c3);
             /*  while (c3 < 12)
             {
-                c3 = c3 + 2;    */
-                Console.WriteLine(c3);
+                c3 = c3 + 2;    
+                Console.WriteLine(c3);  */
             //}
             Console.WriteLine("End of case 3");
 
 
             // Case 4 - Change this to a for-loop!
             Console.WriteLine("Start of case 4");
-            int c4 = 0;
+
+            for (int c4 = 0; c4 < 50; c4 = c4 * 2 + 2)
+                Console.WriteLine(c4);
+
+          /*  int c4 = 0;
             while (c4 < 50)
             {
                 Console.WriteLine(c4);
                 c4 = c4 * 2 + 2;
-            }
+            }   */
             Console.WriteLine("End of case 4");
 
             // The LAST line of code should be ABOVE this line
